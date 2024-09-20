071284ca49ede352dfc6621fbb632f9e224795fd
uttamkn <uttamkn15@gmail.com>
2024-09-20 12:53:31 +0530

    update

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline text-center text-red-400">
+      <h1 className="text-center text-3xl font-bold text-red-400 underline">
         hello world
       </h1>
     </>
